65f32ab3cbdefbd0627ef7cb67fb9043b21079f5
christianbondoc <christianraebondoc@hotmail.com>
2018-04-19 16:37:29 -0700

    Added webapp, and server mongo. install mongo to play around with the db

---
+++ server/index.js
@@ -0,0 +1,35 @@
+var MongoClient = require('mongodb').MongoClient;
+//Create a database named "mydb":
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var hiveDB = db.db("mydb");
+
+    var newUser = {
+        name: 'Christian Bondoc',
+        bcit: 'A00782025',
+        bEmail: 'cbondoc4@bcit.ca',
+        program: 'D3 Digital Design Development'
+    }
+
+    hiveDB.collection("users").insertOne(newUser, (err, res) => {
+        console.log("New User Added");
+        db.close();
+    });
+
+
+
+});
+
+/* Main Docs Example:
+
+Collection name: "users";
+nUser = [{
+    name: 'Christian Bondoc',
+    bcitID: 'A00782025', <-create restrictions only for A00, maximum 9 chars
+    bEmail: 'cbondoc4@bcit.ca' <-- create restrcitions for only @bcit.ca
+    program: 'Digital Design Development' <-- Get from dropdown
+}];
+
+*/
---
+++ webapp/src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './comp/styles/App.css';
+
+class App extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        pgNum: 0
+      };
+    }
+
+
+
+  render() {
+    var comp;
+    var pgNum = this.state.pgNum;
+
+    if (pgNum === 0) {
+      comp = (
+        <div className='wrapper'>
+          <div className="intro-container">
+          <h1 className="mainTxt"> 
+          Register for BCIT Hive </h1>
+            <button className="mainBtn" onClick={((() => { this.setState({ pgNum: 1 }) }))}>Register Now</button> 
+          </div>
+        </div>
+        
+      )
+    }
+    else if (pgNum === 1) {
+      comp = (
+        <div className="wrapper-row">
+          <div className="container left">
+
+            <h1 className="mainTxt">Who are you?</h1>
+            <input className="input" placeholder= "Your Name" />
+            <input className="input" placeholder= "BCIT ID" />
+            <input className="input" placeholder= "BCIT Email" />
+            <input className="input" placeholder= "Program" />
+          </div>
+
+          <button className="searchBtn" onClick={((() => { this.setState({ pgNum: 2 }) }))}> Search Now </button> 
+
+          <div className="container right">
+          <h1 className="mainTxt">Skillset/Program</h1>
+            <div className="content">
+              <h1 className="intro-center"> Looking for a </h1>
+              <select className="positionB">
+                <option value="Graphic Designer"> Graphic Designer </option>
+                <option value="Web Designer"> Web Designer </option>
+                <option value="Software Developer"> Software Developer </option>
+                <option value="Woodworker">Carpentry</option>
+                <option value="Metal Working">Metal Working</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        
+      )}
+      else if (pgNum===2) {
+        comp = (
+          <div className='wrapper'>
+            <div className="intro-container">
+              <h1 className="mainTxt">
+                Thanks for using BCIT Hive </h1>
+              <button className="mainBtn" onClick={((() => { this.setState({ pgNum: 0 }) }))}>Home</button>
+            </div>
+          </div>
+        )
+      } 
+    
+
+
+    return (
+      <div>
+        {comp}
+      </div>
+    );
+  }
+}
+
+export default App;
